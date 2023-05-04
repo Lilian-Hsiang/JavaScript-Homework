57a88501826493d901b4e07657240830884198bc
Lilian-mac <lilian4218@gmail.com>
2023-05-04 13:48:42 +0800

    javascript_HW

---
+++ test.js
@@ -0,0 +1,16 @@
+let bar = document.querySelector('.navbar')
+bar.innerHTML = `<div id="content">
+<nav>
+  <ul>
+    <li><a href="HW-01 BMI.html">BMI</a></li>
+    <li><a href="HW-02 ^畫出聖誕樹.html">聖誕樹</a></li>
+    <li><a href="HW-03 猜數字遊戲.html">猜數字遊戲</a></li>
+    <li><a href="HW-04 閏年.html">閏年</a></li>
+    <li><a href="HW-05 九九乘法表.html">九九乘法表</a></li>
+    <li><a href="HW-07  攝氏華氏溫度.html">溫度轉換器</a></li>
+    <li><a href="HW-08 密碼檢查.html">密碼檢查</a></li>
+    <li><a href="HW-09 檢查電子郵件.html">檢查電子郵件</a></li>
+    <li><a href="HW-10 生日.html">生日</a></li>
+  </ul>
+</nav>
+</div>`
